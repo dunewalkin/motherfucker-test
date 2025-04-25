989e6bab8a75598857d01042df2bda80501ad245
dunewalkin <sergei.gunchenko@gmail.com>
2025-04-25 18:31:29 +0300

    fix routing for Vercel SPA (Vue + Vite)

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
+  base: '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    outDir: 'dist',
   },
   css: {
     preprocessorOptions: {
@@ -22,3 +25,4 @@ export default defineConfig({
     },
   },
 })
+
